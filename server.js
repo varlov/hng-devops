45dcb6bcc9a2f81d9f1532330a6a0028bac60025
varlov <51709867+varlov@users.noreply.github.com>
2026-04-15 18:27:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+const http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>your-hng-username</h1>");
+  }
+
+  else if (req.url === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({
+      message: "HNGI14 Stage 1",
+      track: "DevOps",
+      username: "your-hng-username"
+    }));
+  }
+
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
